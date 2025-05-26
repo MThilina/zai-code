ceae5c6ff656c8c78d3ce6e5ae09ec7e655fb84d
thilinam <thilinamanawadu@gmail.com>
2025-05-26 18:55:02 +0530

    Design of dto classes

---
+++ src/main/java/com/zai/code/weather/dto/WeatherResponse.java
@@ -0,0 +1,13 @@
+package com.zai.code.weather.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class WeatherResponse {
+    private double temperatureDegrees;
+    private double windSpeed;
+}
