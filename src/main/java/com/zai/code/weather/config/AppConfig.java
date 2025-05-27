0d3fd257ca87249b70ee423a0d7c9f0ba09ac68f
thilinam <thilinamanawadu@gmail.com>
2025-05-27 09:06:48 +0530

    Adding caffine cache configuration

--- src/main/java/com/zai/code/weather/config/AppConfig.java
+++ src/main/java/com/zai/code/weather/config/AppConfig.java
@@ -1,9 +1,13 @@
 package com.zai.code.weather.config;
 
+import com.github.benmanes.caffeine.cache.Caffeine;
+import com.zai.code.weather.dto.WeatherResponse;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.client.HttpComponentsClientHttpRequestFactory;
 import org.springframework.web.client.RestTemplate;
+
+import java.util.concurrent.TimeUnit;
 
 @Configuration
 public class AppConfig {
@@ -11,8 +15,15 @@ public class AppConfig {
     @Bean
     public RestTemplate restTemplate() {
         HttpComponentsClientHttpRequestFactory factory = new HttpComponentsClientHttpRequestFactory();
-        factory.setConnectTimeout(3000);
-        factory.setReadTimeout(3000);
+        factory.setConnectTimeout(5000);
+        factory.setReadTimeout(5000);
         return new RestTemplate(factory);
+    }
+
+    @Bean
+    public com.github.benmanes.caffeine.cache.Cache<String, WeatherResponse> weatherCache() {
+        return Caffeine.newBuilder()
+                .expireAfterWrite(3, TimeUnit.SECONDS)
+                .build();
     }
 }
